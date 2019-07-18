a366548944ad84ce122e2412928629eded6c84fd
AlphaCodingCode <52750549+AlphaCodingCode@users.noreply.github.com>
2019-07-18 17:43:41 -0400

    Add files via upload

---
+++ snakeGame.py
@@ -0,0 +1,26 @@
+import pygame
+import random
+
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+
+#set-up the game
+pygame.init()
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption("my Game")
+clock = pygame.time.Clock()
+
+#
+running = True
+while running:
+    # Process input (events)
+    # Update
+    # Render (draw)
+
+# Colors (R, G, B)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
